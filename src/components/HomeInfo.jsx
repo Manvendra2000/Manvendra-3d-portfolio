dbb0d281281e7fd7b0ed2d453c6c9dd3cf9a0383
Manvendra2000 <mzealpro@gmail.com>
2024-09-12 17:43:02 +0530

    text altered

--- src/components/HomeInfo.jsx
+++ src/components/HomeInfo.jsx
@@ -18,7 +18,7 @@ const renderContent = {
         <h1 className='sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5'>
             Hi, I'm <span className='font-semibold'>Manvendra</span>👋
             <br />
-            A software Engineer from India. ⛳
+            A 3D Web Developer from India. ⛳
         </h1>
     ),
 
